4cc48183ebbb3f0f8a13eecd185588d82c1621b3
caiofabiogomes <caiofabio250@gmail.com>
2026-05-23 16:33:07 -0300

    trace

--- KitchenService.API/Program.cs
+++ KitchenService.API/Program.cs
@@ -1,9 +1,31 @@
 using KitchenService.Infrastructure;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var configuration = builder.Configuration;
+
+var serviceName = "FastTechFoods.KitchenServiceAPI";
+
+// Add services to the container
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(serviceName))
+    .WithTracing(tracing =>
+    {
+        tracing
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddSource("MassTransit")
+            .AddOtlpExporter(options =>
+            {
+                // ?? FOR�ANDO A URL E O PROTOCOLO ??
+                options.Endpoint = new Uri("http://otel-collector:4317");
+                options.Protocol = OtlpExportProtocol.Grpc;
+            });
+    });
 
 builder.Services.AddControllers();
 builder.Services.AddInfraestructureModule(configuration);
